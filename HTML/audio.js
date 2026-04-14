c5943d7595b197bb57336caf7fb0f4387d909671
Hrituraj Kauhsik <hriturajkaushik2201@gmail.com>
2026-04-14 14:18:43 +0530

    Java

--- HTML/audio.js
+++ HTML/audio.js
@@ -14,3 +14,20 @@ class car {
 let obj1 = new car("BMW");
 obj1.start();
 obj1.stop();
+
+class car {
+    constructor(brand) {
+        console.log("car name is assigned");
+        this.brand = brand;
+    }
+
+    start() {
+        console.log("start", this.brand);
+    }
+    stop() {
+        console.log("stop", this.brand);
+    }
+}
+let obj1 = new car("BMW");
+obj1.start();
+obj1.stop();
